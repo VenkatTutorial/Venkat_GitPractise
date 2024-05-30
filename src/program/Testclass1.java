9ce15092714474adbae22c009d1ce8be66ecfb71
VenkatTutorial <vent3125@gmail.com>
2024-05-30 10:01:22 +0530

    addeeed

---
+++ src/program/Testclass1.java
@@ -0,0 +1,12 @@
+package src.program;
+
+public class Testclass1 {
+	public static void forwardPattern(int n) {
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
